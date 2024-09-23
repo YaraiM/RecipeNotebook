02fe93d59471d82502c193f7adbdb816e8fadd19
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-09-23 22:56:47 +0900

    RecipeRepositoryの単体テスト：IDを指定して取得するメソッドのテスト、テストメソッド名の修正

--- RecipeNotebook/src/test/java/raisetech/RecipeNotebook/repository/RecipeRepositoryTest.java
+++ RecipeNotebook/src/test/java/raisetech/RecipeNotebook/repository/RecipeRepositoryTest.java
@@ -23,22 +23,31 @@ class RecipeRepositoryTest {
   private RecipeRepository sut;
 
   @Test
-  void レシピの全件検索が行えること() {
+  void レシピを全件取得できること() {
     List<Recipe> actual = sut.getAllRecipes();
 
     assertThat(actual.size(), is(2));
-    assertRecipeDetails(actual.get(0), "卵焼き", "test1/path", "https://------1.com", "2人分",
+    assertRecipeDetail(actual.get(0), "卵焼き", "test1/path", "https://------1.com", "2人分",
         "備考欄1", false, LocalDateTime.parse("2024-09-22T17:00:00"),
         LocalDateTime.parse("2024-10-22T17:00:00"));
-    assertRecipeDetails(actual.get(1), "目玉焼き", "test2/path", "https://------2.com", "1人分",
+    assertRecipeDetail(actual.get(1), "目玉焼き", "test2/path", "https://------2.com", "1人分",
         "備考欄2", true, LocalDateTime.parse("2024-09-23T17:00:00"),
         LocalDateTime.parse("2024-10-23T17:00:00"));
+  }
+
+  @Test
+  void IDに紐づくレシピを取得できること() {
+    Recipe actual = sut.getRecipe(1);
+
+    assertRecipeDetail(actual, "卵焼き", "test1/path", "https://------1.com", "2人分",
+        "備考欄1", false, LocalDateTime.parse("2024-09-22T17:00:00"),
+        LocalDateTime.parse("2024-10-22T17:00:00"));
   }
 
   /**
    * レシピのアサーションを行うヘルパーメソッドです。
    */
-  private void assertRecipeDetails(Recipe recipe, String name, String imagePath, String
+  private void assertRecipeDetail(Recipe recipe, String name, String imagePath, String
       recipeSource, String servings, String remark, Boolean favorite, LocalDateTime
       createdAt, LocalDateTime updatedAt) {
     assertAll(
@@ -56,24 +65,36 @@ class RecipeRepositoryTest {
   }
 
   @Test
-  void 材料の全件検索が行えること() {
+  void 材料を全件取得できること() {
     List<Ingredient> actual = sut.getAllIngredients();
 
     assertThat(actual.size(), is(7));
-    assertIngredientDetails(actual.get(0), 1, "卵", BigDecimal.valueOf(3.0), "個", false);
-    assertIngredientDetails(actual.get(1), 1, "サラダ油", null, null, false);
-    assertIngredientDetails(actual.get(2), 1, "醤油", BigDecimal.valueOf(0.5), "大さじ", false);
-    assertIngredientDetails(actual.get(3), 1, "砂糖", BigDecimal.valueOf(1.0), "大さじ", false);
-    assertIngredientDetails(actual.get(4), 2, "卵", BigDecimal.valueOf(1.0), "個", false);
-    assertIngredientDetails(actual.get(5), 2, "サラダ油", null, null, false);
-    assertIngredientDetails(actual.get(6), 2, "水", null, null, false);
+    assertIngredientDetail(actual.get(0), 1, "卵", BigDecimal.valueOf(3.0), "個", false);
+    assertIngredientDetail(actual.get(1), 1, "サラダ油", null, null, false);
+    assertIngredientDetail(actual.get(2), 1, "醤油", BigDecimal.valueOf(0.5), "大さじ", false);
+    assertIngredientDetail(actual.get(3), 1, "砂糖", BigDecimal.valueOf(1.0), "大さじ", false);
+    assertIngredientDetail(actual.get(4), 2, "卵", BigDecimal.valueOf(1.0), "個", false);
+    assertIngredientDetail(actual.get(5), 2, "サラダ油", null, null, false);
+    assertIngredientDetail(actual.get(6), 2, "水", null, null, false);
+
+  }
+
+  @Test
+  void レシピIDに紐づく材料を取得できること() {
+    List<Ingredient> actual = sut.getIngredients(1);
+
+    assertThat(actual.size(), is(4));
+    assertIngredientDetail(actual.get(0), 1, "卵", BigDecimal.valueOf(3.0), "個", false);
+    assertIngredientDetail(actual.get(1), 1, "サラダ油", null, null, false);
+    assertIngredientDetail(actual.get(2), 1, "醤油", BigDecimal.valueOf(0.5), "大さじ", false);
+    assertIngredientDetail(actual.get(3), 1, "砂糖", BigDecimal.valueOf(1.0), "大さじ", false);
 
   }
 
   /**
    * 材料のアサーションを行うヘルパーメソッドです。
    */
-  private void assertIngredientDetails(Ingredient ingredient, int recipeId, String name,
+  private void assertIngredientDetail(Ingredient ingredient, int recipeId, String name,
       BigDecimal quantity, String
       unit, Boolean arrange) {
     assertAll(
@@ -88,33 +109,47 @@ class RecipeRepositoryTest {
   }
 
   @Test
-  void 調理手順の全件検索が行えること() {
+  void 調理手順を全件取得できること() {
     List<Instruction> actual = sut.getAllInstructions();
 
     assertThat(actual.size(), is(7));
-    assertInstructionDetails(actual.get(0), 1, 1, "卵を溶いて調味料を混ぜ、卵液を作る", false);
-    assertInstructionDetails(actual.get(1), 1, 2, "フライパンに油をたらし、火にかける", false);
-    assertInstructionDetails(actual.get(2), 1, 3, "卵液を1/3くらいフライパンに入れて焼き、巻く",
+    assertInstructionDetail(actual.get(0), 1, 1, "卵を溶いて調味料を混ぜ、卵液を作る", false);
+    assertInstructionDetail(actual.get(1), 1, 2, "フライパンに油をたらし、火にかける", false);
+    assertInstructionDetail(actual.get(2), 1, 3, "卵液を1/3くらいフライパンに入れて焼き、巻く",
         true);
-    assertInstructionDetails(actual.get(3), 1, 4, "3の手順を繰り返して完成", false);
-    assertInstructionDetails(actual.get(4), 2, 1, "フライパンに油をたらし、火にかける", false);
-    assertInstructionDetails(actual.get(5), 2, 2, "フライパンに卵を割り入れる", false);
-    assertInstructionDetails(actual.get(6), 2, 3,
+    assertInstructionDetail(actual.get(3), 1, 4, "3の手順を繰り返して完成", false);
+    assertInstructionDetail(actual.get(4), 2, 1, "フライパンに油をたらし、火にかける", false);
+    assertInstructionDetail(actual.get(5), 2, 2, "フライパンに卵を割り入れる", false);
+    assertInstructionDetail(actual.get(6), 2, 3,
         "少し焼けたら水を入れ、ふたをして5分、弱火にかけて完成", false);
 
   }
 
+  @Test
+  void レシピIDに紐づく調理手順を取得できること() {
+    List<Instruction> actual = sut.getInstructions(1);
+
+    assertThat(actual.size(), is(4));
+    assertInstructionDetail(actual.get(0), 1, 1, "卵を溶いて調味料を混ぜ、卵液を作る", false);
+    assertInstructionDetail(actual.get(1), 1, 2, "フライパンに油をたらし、火にかける", false);
+    assertInstructionDetail(actual.get(2), 1, 3, "卵液を1/3くらいフライパンに入れて焼き、巻く",
+        true);
+    assertInstructionDetail(actual.get(3), 1, 4, "3の手順を繰り返して完成", false);
+
+  }
+
+
   /**
    * 調理手順のアサーションを行うヘルパーメソッドです。
    */
-  private void assertInstructionDetails(Instruction instruction, int recipeId, int stepNumber,
+  private void assertInstructionDetail(Instruction instruction, int recipeId, int stepNumber,
       String content,
       Boolean arrange) {
     assertAll(
         "Multiple assertions",
         () -> assertThat(instruction.getRecipeId(), is(recipeId)),
         () -> assertThat(instruction.getStepNumber(), is(stepNumber)),
-        () -> assertThat(instruction.getInstruction(), is(content)),
+        () -> assertThat(instruction.getContent(), is(content)),
         () -> assertThat(instruction.getArrange(), is(arrange))
     );
 
