ad4d0f0a85f04e4502f6c5ba4407d879109ab4c8
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-10-30 16:42:13 +0900

    画像ファイルのアップロードに対応できるようActions周りを修正

--- RecipeNotebook/src/main/java/raisetech/RecipeNotebook/config/WebConfig.java
+++ RecipeNotebook/src/main/java/raisetech/RecipeNotebook/config/WebConfig.java
@@ -2,10 +2,12 @@ package raisetech.RecipeNotebook.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("!ci")
 public class WebConfig implements WebMvcConfigurer {
 
   @Value("${app.upload.dir}")
