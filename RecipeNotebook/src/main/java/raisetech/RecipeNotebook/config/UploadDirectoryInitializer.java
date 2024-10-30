c48c4fffe2699d62a1add99327f1694b43909dee
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-10-30 16:05:05 +0900

    画像ファイルのアップロードに対応できるようActions周りを修正

---
+++ RecipeNotebook/src/main/java/raisetech/RecipeNotebook/config/UploadDirectoryInitializer.java
@@ -0,0 +1,27 @@
+package raisetech.RecipeNotebook.config;
+
+import jakarta.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("!ci")  // ciプロファイルではこのBeanを無効化
+public class UploadDirectoryInitializer {
+
+  @Value("${app.upload.dir}")
+  private String uploadDir;
+
+  @PostConstruct
+  public void init() {
+    try {
+      Files.createDirectories(Paths.get(uploadDir));
+      System.out.println("Upload directory: " + Paths.get(uploadDir).toAbsolutePath());
+    } catch (IOException e) {
+      throw new RuntimeException("Could not create upload directory!", e);
+    }
+  }
+}
