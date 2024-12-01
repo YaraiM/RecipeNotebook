52adca6ab47135a0145fe115af637e6f632461bd
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-12-01 14:04:15 +0900

    データベースに初期サンプルデータを追加

--- RecipeNotebook/sql/001-create-table-and-load-data.sql
+++ RecipeNotebook/sql/001-create-table-and-load-data.sql
@@ -13,11 +13,12 @@ CREATE TABLE recipes (
   PRIMARY KEY(id)
 );
 
-INSERT INTO recipes (name, image_path, recipe_source, servings, remark, created_at, updated_at)
-VALUES ('卵焼き', '/images/tamagoyaki.jpg', 'https://------1.com', '2人分', '備考欄1', '2024-09-22 17:00:00', '2024-10-22 17:00:00');
 INSERT INTO recipes (name, image_path, recipe_source, servings, remark, favorite, created_at, updated_at)
-VALUES ('目玉焼き', '/images/medamayaki.jpg', 'https://------2.com', '1人分', '備考欄2', TRUE, '2024-09-23 17:00:00', '2024-10-23 17:00:00');
-
+VALUES
+('卵焼き', '/images/tamagoyaki.jpg', null, '2人前', '基本的な卵焼きのレシピ。', FALSE,'2024-09-22 17:00:00', '2024-10-22 17:00:00'),
+('目玉焼き', '/images/medamayaki.jpg', null, '1人前', '基本的な目玉焼きのレシピ。', FALSE, '2024-09-23 17:00:00', '2024-10-23 17:00:00'),
+('ナポリタン', '/images/neapolitan.jpg', 'https://www.youtube.com/watch?v=uCbg_uS9aEU&t=452s', '1人前', '料理研究家リュウジさんのレシピをベースに、アレンジを加えたナポリタンのレシピ。ケチャップ減らして醤油とウスターソースを追加して甘さ控えめ・キレのある味わい。', TRUE, '2024-12-01 11:00:00', null),
+('ジャージャー風・肉味噌', '/images/jaja_nikumiso.jpg', 'https://www.youtube.com/watch?v=FwoGQ6GlXQs', '7～8食分', '「くまの限界食堂」さんのレシピのうち、肉味噌部分を抜き出してアレンジを加えたレシピ。人参少し足して栄養と甘味追加。にんにくしょうがはみじん切りで炒める。作り置きに最適。', TRUE, '2024-12-01 11:00:00', null);
 
 DROP TABLE IF EXISTS ingredients;
 
@@ -34,8 +35,14 @@ CREATE TABLE ingredients (
 INSERT INTO ingredients (recipe_id, name, quantity, arrange)
 VALUES
 (1, '卵', '3個', FALSE), (1, 'サラダ油', '適量', FALSE), (1, '醤油', '大さじ1/2', FALSE), (1, '砂糖', '大さじ1', FALSE),
-(2, '卵', '1個', FALSE), (2, 'サラダ油', '適量', FALSE), (2, '水', NULL, FALSE);
-
+(2, '卵', '1個', FALSE), (2, 'サラダ油', '適量', FALSE), (2, '水', NULL, FALSE),
+(3, 'パスタ', '100g', FALSE), (3, 'マッシュルーム', '2個', FALSE), (3, 'ウインナー', '3本', FALSE), (3, '玉ねぎ', '50g', FALSE),
+(3, 'ピーマン', '1個', FALSE), (3, 'ケチャップ', '大さじ3', TRUE), (3, '醤油', '小さじ1', TRUE), (3, 'ウスターソース', '小さじ1', TRUE),
+(3, 'バター', '10g', FALSE), (3, 'オリーブオイル', '適量', TRUE), (3, '塩', '適量', FALSE), (3, '粉チーズ', '適量', FALSE),
+(4, '豚ひき肉', '600g', FALSE), (4, 'しいたけ', '6本', FALSE), (4, 'ネギ', '1本', FALSE),(4, '人参', '1/3本', TRUE),
+(4, 'Aオイスターソース', '大さじ3', FALSE),(4, 'A味噌', '大さじ3', FALSE),(4, 'A酒', '大さじ3', FALSE),(4, 'A砂糖', '大さじ3', FALSE),
+(4, 'しいたけ', '6本', FALSE),(4, 'にんにく', '30g', TRUE),(4, 'しょうが', '30g', TRUE),(4, 'B醤油', '大さじ1', FALSE),
+(4, 'Bごま油', '大さじ2~3', FALSE), (4, 'サラダ油', '適量', FALSE);
 
 DROP TABLE IF EXISTS instructions;
 
@@ -54,8 +61,13 @@ VALUES
 (1, 1, '卵を溶いて調味料を混ぜ、卵液を作る', FALSE), (1, 2, 'フライパンに油をたらし、火にかける', FALSE),
 (1, 3, '卵液を1/3くらいフライパンに入れて焼き、巻く', TRUE), (1, 4, '3の手順を繰り返して完成', FALSE),
 (2, 1, 'フライパンに油をたらし、火にかける', FALSE), (2, 2, 'フライパンに卵を割り入れる', FALSE),
-(2, 3, '少し焼けたら水を入れ、ふたをして5分、弱火にかけて完成', FALSE);
-
+(2, 3, '少し焼けたら水を入れ、ふたをして5分、弱火にかけて完成', FALSE),
+(3, 1, '材料をすべてスライスする', FALSE), (3, 2, 'パスタを茹で始める', FALSE), (3, 3, '少量のサラダ油でウインナーを炒める', FALSE),
+(3, 4, '玉ねぎをしんなりするまで炒める', FALSE),(3, 5, 'すべての調味料とマッシュルームを入れてしばらく炒める', TRUE),
+(3, 6, 'バターとピーマンを入れて少し炒める', FALSE),(3, 7, 'パスタとソースを合わせて焼く', FALSE),(3, 8, '皿に盛ってチーズ振って完成', FALSE),
+(4, 1, '長ネギ、しいたけ、にんにく、しょうがをみじん切りにする', FALSE), (4, 2, 'サラダ油で長ネギを香りが出るまで炒めてネギ油を作る', FALSE),
+(4, 3, 'にんにく、しょうがを香りが出るまで炒めたら肉を入れる', TRUE),(4, 4, '肉に火が通ったら人参とシイタケを入れて少し炒める', TRUE),
+(4, 5, 'Aの調味料を入れて煮詰める', FALSE),(4, 6, 'Bの調味料を入れて1分ほど熱したら完成', FALSE);
 
 DROP TABLE IF EXISTS categories;
 
