acbbd446bc1fc8e5cef27cfb35db36d93ade9abc
YaraiM <hara_kiri1990@yahoo.co.jp>
2024-12-10 03:09:57 +0900

    API仕様書の修正

--- RecipeNotebook/src/main/java/raisetech/RecipeNotebook/RecipeNotebookApplication.java
+++ RecipeNotebook/src/main/java/raisetech/RecipeNotebook/RecipeNotebookApplication.java
@@ -10,12 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @OpenAPIDefinition(
     info = @Info(
         title = "レシピノートAPI仕様",
-        description = "レシピノートアプリのAPI仕様書です。APIの動作確認を行う場合は、Basic認証およびCSRFトークンの送信が必要です。",
-        version = "0.1.0",
+        description = "レシピノートアプリのAPI仕様書です。",
+        version = "1.0.0",
         license = @License(name = "Apache License 2.0", url = "https://www.apache.org/licenses/LICENSE-2.0.txt")),
     servers = {
-        @Server(description = "Development Server", url = "http://localhost:8080"),
-        @Server(description = "Production Server", url = "-")
+        @Server(description = "Development Server", url = "http://localhost:8080")
     }
 )
 @SpringBootApplication
